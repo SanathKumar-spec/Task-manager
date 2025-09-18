9585e405f9eb7f512d34de64f40f2e9b15ffeefc
SanathKumar-spec <sanath77139@gmail.com>
2025-09-18 19:57:01 +0530

    issues reesolved

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://task-manager-hv7auqfp0-sanath-kumars-projects-02eccfe3.vercel.app",
-  "https://68cc14a15315c9403560bb73--kaleidoscopic-clafoutis-640523.netlify.app/",
+  origin: "https://68cc14a15315c9403560bb73--kaleidoscopic-clafoutis-640523.netlify.app/",
   credentials: true
 }));
 
